3ade55d1e6635957f63b4d185eb22740673d3e73
EumirGonzales <e.gonzales@codeshop.io>
2016-06-27 16:59:57 +0800

    contact us now replies to given email using mailgun

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var mailService = require('../services/mail.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +9,22 @@ router.get('/', function(req, res, next) {
 
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Express' });
+});
+
+router.post('/contact', function(req, res, next) {
+  var input = req.body;
+  if(input.email){
+  	mailService.sendContactEmail(input.email,input.name, function(error,body){  		
+  		if(error){
+  			console.log('Error in sending mail:',error);
+
+  		}else{
+  			console.log('Success sending mail', body);
+  			res.send("Okay");
+  		}
+  	})
+  }
+  
 });
 
 module.exports = router;
